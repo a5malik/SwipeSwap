bdb2eda3b5c6a86c2e7a8022a40d454514f8ed6f
tsleeve <akash5malik@gmail.com>
2016-10-21 23:09:45 -0700

    calendar view with 1 month window

--- app/src/main/java/com/example/tsleeve/swipeswap/CalendarMonthFragment.java
+++ app/src/main/java/com/example/tsleeve/swipeswap/CalendarMonthFragment.java
@@ -2,18 +2,23 @@ package com.example.tsleeve.swipeswap;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CalendarMonthFragment extends Fragment {
+public class CalendarMonthFragment extends Fragment implements CalendarView.OnDateChangeListener {
 
-
+    CalendarView calendarView;
     public CalendarMonthFragment() {
         // Required empty public constructor
     }
@@ -26,4 +31,18 @@ public class CalendarMonthFragment extends Fragment {
         return inflater.inflate(R.layout.calendar_month_fragment, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(this);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 1);
+        calendarView.setMaxDate(calendar.getTimeInMillis());
+        calendarView.setMinDate(Calendar.getInstance().getTimeInMillis());
+    }
+
+    @Override
+    public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+    }
 }
