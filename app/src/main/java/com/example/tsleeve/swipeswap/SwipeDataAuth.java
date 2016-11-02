02ed1167b424fad9740efdf8b462c4fa608b618e
Akshay Bhat <akshaybhat214@gmail.com>
2016-11-02 01:55:38 -0700

    Added basic documentation to SwipeDataAuth.java

--- app/src/main/java/com/example/tsleeve/swipeswap/SwipeDataAuth.java
+++ app/src/main/java/com/example/tsleeve/swipeswap/SwipeDataAuth.java
@@ -81,25 +81,59 @@ public class SwipeDataAuth {
 
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
+    /**
+     * This function writes a swipe obect to mDatabase
+     * under both root->swipes and root->users->uid->swipes
+     *
+     * @param s The Swipe object to be written to Firebase
+     * @param uid User ID of the swipe seller
+     * @return
+     */
     public Task<Void> addSwipe(Swipe s, String uid) {
         mDatabase.child(ALL_USERS).child(uid).child(ALL_SWIPES).push().setValue(s);
         return mDatabase.child(ALL_SWIPES).push().setValue(s);
     }
 
+    /**
+     * This function writes a swipe object to mDatabase
+     * under root->requests
+     *
+     * @param s The Swipe object to be written to Firebase
+     * @return
+     */
     public Task<Void> addRequest(Swipe s) {
         return mDatabase.child(ALL_REQUESTS).push().setValue(s);
     }
 
     // TODO: Remove swipe
 
+    /**
+     * Gets a reference to the root->users in the Firebase data
+     *
+     * @return reference to the Google Cloud Storage object that has all users data
+     */
     public DatabaseReference getUsersReference() {
         return mDatabase.child(ALL_USERS);
     }
 
+    /**
+     * Returns the reference to a specific user's reference,
+     * and can be used to obtain the users metadata.
+     *
+     * @param uid UserID of the user whose reference we want
+     * @return Reference to the Google Cloud Storage object of the user
+     */
     public DatabaseReference getUserReference(String uid) {
         return mDatabase.child(ALL_USERS).child(uid);
     }
 
+    /**
+     * Set a username under root->users->uid->username
+     *
+     * @param uid New UserID
+     * @param username New Username
+     * @return
+     */
     public Task<Void> registerUsername(String uid, String username) {
         return mDatabase.child(ALL_USERS).child(uid).child(USERNAME).setValue(username);
     }
@@ -109,12 +143,27 @@ public class SwipeDataAuth {
         return ref.orderByChild(key).startAt(start).endAt(end);
     }
 
+    /**
+     * Sets a value for root->users->uid->regToken in the firebase data.
+     * regToken is used for user authentication, and is stored in user objects.
+     *
+     * @param token registration token to be updated for user
+     * @param uid UserID to update user's Google Cloud Storage object
+     * @return
+     */
     public Task<Void> updateToken(String token, String uid) {
         if (uid == null)
             return null;
         return mDatabase.child(ALL_USERS).child(uid).child(TOKEN).setValue(token);
     }
 
+    /**
+     * Gets the registration token of a user from root->users->uid->regToken
+     * in firebase, and sleeps 1 second to recieve the data.
+     *
+     * @param uid UserID to obtain the registration token
+     * @return the user regToken in string format
+     */
     public String getUserToken(String uid) {
         DatabaseReference ref = mDatabase.child(ALL_USERS).child(uid).child(TOKEN);
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -139,6 +188,15 @@ public class SwipeDataAuth {
         return mUserToken;
     }
 
+    /**
+     * This function first ets a reference to root->users->uid->swipes for a
+     * particular user ID, and adds all the swipes associated with this reference to an
+     * Arraylist, using data snapshots, to map each attribute in firebase to
+     * the corresponding field in the Swipes class
+     *
+     * @param uid UserID of the firebase user
+     * @return ArrayList of all the swipes associated with a particular user
+     */
     public ArrayList<Swipe> getAllSwipesByUser(String uid) {
         DatabaseReference ref = mDatabase.child(ALL_USERS).child(uid).child(ALL_SWIPES);
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
